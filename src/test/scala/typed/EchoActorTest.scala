1039b73d8b06be448a55905f9f569a26bc1e7ed3
objektwerks <objektwerks@runbox.com>
2019-06-28 16:15:23 -0400

    refactored behavior signature for echo actor

--- src/test/scala/typed/EchoActorTest.scala
+++ src/test/scala/typed/EchoActorTest.scala
@@ -1,15 +1,15 @@
 package typed
 
 import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
+import akka.actor.typed.ActorRef
 import akka.actor.typed.scaladsl.Behaviors
-import akka.actor.typed.{ActorRef, Behavior}
 import org.scalatest.WordSpecLike
 
 object EchoActor {
   case class Message(text: String, sender: ActorRef[Echo])
   case class Echo(text: String)
 
-  val echoActorBehavior: Behavior[Message] = Behaviors.receive { (context, message) =>
+  val echoActorBehavior = Behaviors.receive[Message] { (context, message) =>
     message match {
       case Message(text, sender) =>
         context.log.info("Message.text = {} from {}", text, sender.path.name)
