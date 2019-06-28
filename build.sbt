cb4d0508b7b88163962ebab8b62302304c1f39d8
objektwerks <objektwerks@runbox.com>
2019-06-28 13:52:02 -0400

    corrected logging; log in echo actor

--- build.sbt
+++ build.sbt
@@ -6,6 +6,7 @@ libraryDependencies ++= {
   val akkaVersion = "2.5.23"
   Seq(
     "com.typesafe.akka" %% "akka-actor-typed" % akkaVersion,
+    "com.typesafe.akka" %% "akka-slf4j" % akkaVersion,
     "ch.qos.logback" % "logback-classic" % "1.2.3",
     "com.typesafe.akka" %% "akka-actor-testkit-typed" % akkaVersion % Test,
     "org.scalatest" %% "scalatest" % "3.0.5" % Test
--- src/test/scala/typed/EchoActorTest.scala
+++ src/test/scala/typed/EchoActorTest.scala
@@ -1,17 +1,20 @@
 package typed
 
 import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
-import akka.actor.typed.{ActorRef, Behavior}
 import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.{ActorRef, Behavior}
 import org.scalatest.WordSpecLike
 
 object EchoActor {
+
   case class Message(text: String, sender: ActorRef[Echo])
+
   case class Echo(text: String)
 
-  val echoBehavior: Behavior[Message] = Behaviors.receive {
-    (_, message) => message match {
+  val echoBehavior: Behavior[Message] = Behaviors.receive { (context, message) =>
+    message match {
       case Message(text, sender) =>
+        context.log.info("Message.text = {}", text)
         sender ! Echo(text)
         Behaviors.same
     }
