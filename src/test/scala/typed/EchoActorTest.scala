9d2d2e192e29f0e55560f3818f9fdaeb4c0d9b5e
objektwerks <objektwerks@runbox.com>
2019-06-28 16:12:48 -0400

    corrected echo actor

--- src/test/scala/typed/EchoActorTest.scala
+++ src/test/scala/typed/EchoActorTest.scala
@@ -2,24 +2,20 @@ package typed
 
 import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
 import akka.actor.typed.scaladsl.Behaviors
-import akka.actor.typed.{ActorRef, Behavior, PostStop}
+import akka.actor.typed.{ActorRef, Behavior}
 import org.scalatest.WordSpecLike
 
 object EchoActor {
   case class Message(text: String, sender: ActorRef[Echo])
   case class Echo(text: String)
 
-  val echoBehavior: Behavior[Message] = Behaviors.receive { (context, message) =>
+  val echoActorBehavior: Behavior[Message] = Behaviors.receive { (context, message) =>
     message match {
       case Message(text, sender) =>
         context.log.info("Message.text = {} from {}", text, sender.path.name)
         sender ! Echo(text)
         Behaviors.same
     }
-  }.receiveSignal {
-    case (context, PostStop) =>
-      context.log.info("EchoActor stopped!")
-      Behaviors.same
   }
 }
 
@@ -29,7 +25,7 @@ class EchoActorTest extends ScalaTestWithActorTestKit with WordSpecLike {
   "Echo actor behavior" should {
     "echo message" in {
       val testProbe = createTestProbe[Echo]("test-probe")
-      val echoActor = spawn(echoBehavior, "echo-actor")
+      val echoActor = spawn(echoActorBehavior, "echo-actor")
       echoActor ! Message("ping", testProbe.ref)
       testProbe.expectMessage(Echo("ping"))
     }
