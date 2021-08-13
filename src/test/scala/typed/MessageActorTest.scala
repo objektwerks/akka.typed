68d391826eff39984aa234d2f75f543b8279d800
objektwerks <objektwerks@runbox.com>
2021-08-13 07:42:43 -0400

    refactored message actor test

--- src/test/scala/typed/MessageActorTest.scala
+++ src/test/scala/typed/MessageActorTest.scala
@@ -5,13 +5,14 @@ import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.{ActorRef, PostStop}
 
 import org.scalatest.wordspec.AnyWordSpecLike
+import akka.actor.typed.Behavior
+
+sealed trait Entity extends Product with Serializable
+final case class Message(text: String, sender: ActorRef[Echo]) extends Entity
+final case class Echo(text: String) extends Entity
 
 object MessageActor {
-  sealed trait Entity extends Product with Serializable
-  final case class Message(text: String, sender: ActorRef[Echo]) extends Entity
-  final case class Echo(text: String) extends Entity
-
-  val echoActorBehavior = Behaviors.receive[Message] { (context, message) =>
+  def apply(): Behavior[Message] = Behaviors.receive[Message] { (context, message) =>
     message match {
       case Message(text, sender) =>
         context.log.info("*** Message.text = {} from {}", text, sender.path.name)
@@ -26,12 +27,10 @@ object MessageActor {
 }
 
 class MessageActorTest extends ScalaTestWithActorTestKit with AnyWordSpecLike {
-  import MessageActor._
-
   "MessageActor behavior" should {
     "message / echo" in {
       val testProbe = createTestProbe[Echo]("test-probe")
-      val messageActor = spawn(echoActorBehavior, "message-actor")
+      val messageActor = spawn(MessageActor(), "message-actor")
       messageActor ! Message("test", testProbe.ref)
       testProbe.expectMessage(Echo("test"))
     }
