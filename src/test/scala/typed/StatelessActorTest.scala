3ae147e8ff4bc64aa3c942a2d83968930f977eed
objektwerks <objektwerks@runbox.com>
2021-08-13 07:39:23 -0400

    refactored stateless actor test

--- src/test/scala/typed/StatelessActorTest.scala
+++ src/test/scala/typed/StatelessActorTest.scala
@@ -1,37 +1,34 @@
 package typed
 
+import akka.actor.typed.Behavior
+import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
 
 import org.scalatest.wordspec.AnyWordSpecLike
 
+sealed trait Count extends Product with Serializable
+case object Increment extends Count
+case object Decrement extends Count
+
 object CountActor {
-  import akka.actor.typed.Behavior
-  import akka.actor.typed.scaladsl.Behaviors
-
-  sealed trait Count extends Product with Serializable
-  case object Increment extends Count
-  case object Decrement extends Count
-
-  def onOffActorBehavior(count: Int = 0): Behavior[Count] =
+  def behavior(count: Int = 0): Behavior[Count] =
     Behaviors.receive { (context, message) =>
       message match {
         case Increment =>
           context.log.info(s"*** Increment +1, Count: ${count + 1}")
-          onOffActorBehavior(count + 1)
+          behavior(count + 1)
         case Decrement =>
           context.log.info(s"*** Decrement -1, Count: ${count - 1}")
-          onOffActorBehavior(count - 1)
+          behavior(count - 1)
       }
     }
-  }
+}
 
 class StatelessActorTest extends ScalaTestWithActorTestKit with AnyWordSpecLike {
-  import CountActor._
-
   "CountActor behavior" should {
     "increment / decrement" in {
       val testProbe = createTestProbe[Count]("test-count")
-      val countActor = spawn(onOffActorBehavior(0), "count-actor")
+      val countActor = spawn(CountActor.behavior(0), "count-actor")
       countActor ! Increment
       testProbe.expectNoMessage()
       countActor ! Decrement
