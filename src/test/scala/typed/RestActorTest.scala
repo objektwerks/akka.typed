aed8dc6b734e372a51004f867957ff38d41b1b8a
objektwerks <objektwerks@runbox.com>
2021-08-13 07:57:58 -0400

    refactored rest actor test

--- src/test/scala/typed/RestActorTest.scala
+++ src/test/scala/typed/RestActorTest.scala
@@ -1,10 +1,9 @@
 package typed
 
-import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
-import akka.actor.typed.ActorRef
-import akka.actor.typed.Behavior
-import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.ActorSystem
+import akka.actor.typed.{ActorRef, Behavior}
+import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
 
 import akka.http.scaladsl.unmarshalling.Unmarshal
 import akka.http.scaladsl.Http
@@ -17,6 +16,10 @@ import org.scalatest.wordspec.AnyWordSpecLike
 import scala.util.{Success, Failure}
 import scala.concurrent.ExecutionContext
 import scala.concurrent.Future
+
+sealed trait Communique extends Product with Serializable
+final case class GetJoke(sender: ActorRef[Joke]) extends Communique
+final case class Joke(text: String, sender: ActorRef[Joke]) extends Communique
 
 object Rest {
   implicit lazy val formats = DefaultFormats
@@ -37,18 +40,10 @@ object Rest {
   }
 }
 
-object Messages {
-  sealed trait Message extends Product with Serializable
-  final case class GetJoke(sender: ActorRef[Joke]) extends Message
-  final case class Joke(text: String, sender: ActorRef[Joke]) extends Message
-}
-
-object RestActor {  
-  import Messages._
-
-  def restActorBehavior(implicit system: ActorSystem, dispatcher: ExecutionContext): Behavior[Message] =
-    Behaviors.receive { (context, message) =>
-      message match {
+object RestActor {
+  def apply(implicit system: ActorSystem, dispatcher: ExecutionContext): Behavior[Communique] =
+    Behaviors.receive[Communique] { (context, communique) =>
+      communique match {
         case GetJoke(sender) =>
           context.log.info("*** GetJoke for {}", sender.path.name)
           context.pipeToSelf( Rest.getJoke ) {
@@ -68,13 +63,10 @@ class RestActorTest extends ScalaTestWithActorTestKit with AnyWordSpecLike {
   implicit override val system = testKit.internalSystem
   implicit val dispatcher = system.executionContext
 
-  import Messages._
-  import RestActor._
-
   "RestActor behavior" should {
     "getJoke / joke" in {
-      val testProbe = createTestProbe[Message]("test-rest")
-      val restActor = spawn(restActorBehavior(system.classicSystem, dispatcher), "rest-actor")
+      val testProbe = createTestProbe[Communique]("test-rest")
+      val restActor = spawn(RestActor(system.classicSystem, dispatcher), "rest-actor")
       restActor ! GetJoke(testProbe.ref)
       testProbe.expectMessageType[Joke]
     }
