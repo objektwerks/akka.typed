fc09356737296f231cbb659ccbfd0d9067f9452c
objektwerks <objektwerks@runbox.com>
2021-08-15 08:46:11 -0400

    further refactored factorial app

--- src/main/scala/typed/FactorialApp.scala
+++ src/main/scala/typed/FactorialApp.scala
@@ -6,8 +6,8 @@ import akka.actor.typed.scaladsl.Behaviors
 import scala.annotation.tailrec
 
 sealed trait Calculation extends Product with Serializable
-final case class Numbers(numbers: List[Long]) extends Calculation
-final case class CalculateFactorials(numbers: List[Long], sender: ActorRef[FactorialsCalculated]) extends Calculation
+final case class Calculate(numbers: List[Long]) extends Calculation
+final case class CalculateFactorials(numbers: List[Long], sender: ActorRef[Calculation]) extends Calculation
 final case class FactorialsCalculated(numbers: List[Long]) extends Calculation
 
 object FactorialActor {
@@ -35,14 +35,13 @@ object FactorialActor {
 object CalculationActor {
   def apply(): Behavior[Calculation] = Behaviors.setup { context =>
     val factorialActor = context.spawn(FactorialActor(), "factorial-actor")
-    context.log.info("*** CalculationActor started!")
-    context.watch(factorialActor)
+    context.log.info("*** FactorialActor started!")
 
     Behaviors.receive[Calculation] {
       (context, message) =>
         message match {
-          case Numbers(numbers) =>
-            context.log.info("*** Numbers = {}", numbers)
+          case Calculate(numbers) =>
+            context.log.info("*** Calculate numbers = {}", numbers)
             factorialActor ! CalculateFactorials(numbers, context.self)
             Behaviors.same
           case FactorialsCalculated(numbers) =>
@@ -61,12 +60,11 @@ object CalculationActor {
 object FactorialApp {
   def main(args: Array[String]): Unit = {
     val system = ActorSystem[Calculation](CalculationActor(), "factorial-app")
+    system.log.info("*** CalculationActor started!")
     system.log.info("*** FactorialApp running!")
-    system ! Numbers(List[Long](1, 2, 3))
-    Thread.sleep(1000L)
-    system ! Numbers(List[Long](4, 5, 6))
-    Thread.sleep(1000L)
-    system ! Numbers(List[Long](7, 8, 9))
+    system ! Calculate(List[Long](3, 4, 5))
+    system ! Calculate(List[Long](6, 7, 8))
+    system ! Calculate(List[Long](9, 10, 11))
     Thread.sleep(1000L)
     system.log.info("*** FactorialApp terminating ...")
     system.terminate()
