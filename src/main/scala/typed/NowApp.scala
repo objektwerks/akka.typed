13efba927d4e6abbc2a926aee0f0ee3fb58b0485
objektwerks <objektwerks@runbox.com>
2021-08-16 08:01:14 -0400

    added now app

---
+++ src/main/scala/typed/NowApp.scala
@@ -0,0 +1,20 @@
+package typed
+
+import akka.actor.typed.ActorSystem
+import akka.actor.typed.scaladsl.Behaviors
+
+import java.time.Instant
+
+object NowApp {
+  def main(args: Array[String]): Unit = {
+    final case class Now(now: String = Instant.now.toString) extends Product with Serializable
+    val nowActor = Behaviors.receiveMessage[Now] {
+      case Now(now) =>
+        println(s"*** Now $now")
+        Behaviors.same
+    }
+    val system = ActorSystem[Now](nowActor, "now-app")
+    system ! Now()
+    system.terminate()
+  }
+}
